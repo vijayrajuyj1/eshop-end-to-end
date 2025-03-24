7a70a28e7582960cc61184e633e94ee0c1496dfc
vijayrajuyj1 <134908434+vijayrajuyj1@users.noreply.github.com>
2025-03-24 12:02:42 +0530

    Update get_helm.sh

--- helm/get_helm.sh
+++ helm/get_helm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright The Helm Authors.
+# Copyright The Helm Authors to get.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
